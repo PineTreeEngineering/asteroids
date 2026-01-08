3efdd55ddeda5cd65d99e02524203db5630090a5
PineTreeEngineering <pinetreeengineering@gmail.com>
2026-01-08 05:36:34 +0100

    fps and circle

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -19,6 +23,8 @@ def main():
 
         pygame.Surface.fill(screen, black)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
